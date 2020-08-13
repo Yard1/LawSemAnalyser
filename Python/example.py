585c46f2bb7056018a881df55f53279de0b1fe95
Antoni Baum <antoni.baum@protonmail.com>
2020-08-13 17:00:35 +0200

    Add an option to bypass docker step

--- Python/example.py
+++ Python/example.py
@@ -1,5 +1,5 @@
 from LawSemAnalyser import SemAnalyser
 
 # SemAnalyser("ścieżka/do/folderu/wyjściowego", "ścieżka/do/folderu/wejściowego")
-analyser = SemAnalyser("out", "example_html")
+analyser = SemAnalyser("../out", "../prawo_konsumenckie_html", docker_image=None)
 analyser.analyse_docs()
